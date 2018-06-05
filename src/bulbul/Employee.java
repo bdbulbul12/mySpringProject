d1053607f0bb1588fb9626599791a8f5067fc1cd
bdbulbul <bulbulahmed.cse@yahoo.com>
2018-06-05 14:59:45 +0600

    tutorial 9 has been completed

--- src/bulbul/Employee.java
+++ src/bulbul/Employee.java
@@ -10,7 +10,6 @@ import java.util.List;
 public class Employee {
     
     private List<Address> address1;
-    private List<Address> address2;
 
     public List<Address> getAddress1() {
         return address1;
@@ -19,20 +18,10 @@ public class Employee {
     public void setAddress1(List<Address> address1) {
         this.address1 = address1;
     }
-
-    public List<Address> getAddress2() {
-        return address2;
-    }
-
-    public void setAddress2(List<Address> address2) {
-        this.address2 = address2;
-    }
-
    
-    
-    
+     
     public void show(){
         System.out.println(address1);
-        System.out.println(address2);
+ 
     }
 }
