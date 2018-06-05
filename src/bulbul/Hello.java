30e2dfe0b876b1c5ee596fbcf27915fc853850c9
bdbulbul <bulbulahmed.cse@yahoo.com>
2018-06-05 12:14:38 +0600

    commit all file

---
+++ src/bulbul/Hello.java
@@ -0,0 +1,17 @@
+
+package bulbul;
+
+
+public class Hello {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    
+}
---
+++ src/bulbul/TestEmployee.java
@@ -0,0 +1,15 @@
+package bulbul;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class TestEmployee {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+        "bulbul/applicationContext.xml");
+        
+        Employee emp = (Employee) context.getBean("e2");
+        emp.show();
+    }
+}
